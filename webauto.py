94cc1944d0d6615dc7c01d01f4b1c619babb47f4
Punyaslok Dutta <punyaslokdutta@gmail.com>
2017-07-30 15:27:03 +0530

    Add files via upload

---
+++ webauto.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Chrome()
+driver.get("https://www.youtube.com/watch?v=A5_XdiK4J8A&list=PL2_aWCzGMAwI3W_JlcBbtYTwiQSsOTa6P&index=24")
